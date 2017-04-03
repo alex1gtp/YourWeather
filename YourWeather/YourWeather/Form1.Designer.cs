df7b52f7d3e63b784c3b31144f440abec4ace893
alex1gtp <alex1gtp@gmail.com>
2017-04-03 20:02:25 +0300

    Start Project
    !! Problem with push data from server////
    Working on it

---
+++ YourWeather/YourWeather/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace YourWeather
+{
+    partial class formWeather
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxCities = new System.Windows.Forms.ComboBox();
+            this.comboBoxCountry = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxWeatherDetails = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxCities);
+            this.groupBox1.Controls.Add(this.comboBoxCountry);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(594, 109);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // comboBoxCities
+            // 
+            this.comboBoxCities.FormattingEnabled = true;
+            this.comboBoxCities.Location = new System.Drawing.Point(163, 70);
+            this.comboBoxCities.Name = "comboBoxCities";
+            this.comboBoxCities.Size = new System.Drawing.Size(393, 21);
+            this.comboBoxCities.TabIndex = 3;
+            this.comboBoxCities.SelectedIndexChanged += new System.EventHandler(this.comboBoxCities_SelectedIndexChanged);
+            // 
+            // comboBoxCountry
+            // 
+            this.comboBoxCountry.FormattingEnabled = true;
+            this.comboBoxCountry.Location = new System.Drawing.Point(163, 21);
+            this.comboBoxCountry.Name = "comboBoxCountry";
+            this.comboBoxCountry.Size = new System.Drawing.Size(393, 21);
+            this.comboBoxCountry.TabIndex = 2;
+            this.comboBoxCountry.SelectedIndexChanged += new System.EventHandler(this.comboBoxCountry_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cities";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Countries";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBoxWeatherDetails);
+            this.groupBox2.Location = new System.Drawing.Point(0, 115);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(594, 281);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            // 
+            // richTextBoxWeatherDetails
+            // 
+            this.richTextBoxWeatherDetails.Location = new System.Drawing.Point(16, 0);
+            this.richTextBoxWeatherDetails.Name = "richTextBoxWeatherDetails";
+            this.richTextBoxWeatherDetails.Size = new System.Drawing.Size(540, 245);
+            this.richTextBoxWeatherDetails.TabIndex = 0;
+            this.richTextBoxWeatherDetails.Text = "";
+            // 
+            // formWeather
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 395);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "formWeather";
+            this.Text = "YourWeather";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxCities;
+        private System.Windows.Forms.ComboBox comboBoxCountry;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBoxWeatherDetails;
+    }
+}
+
---
+++ YourWeather/YourWeather/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using YourWeather.ServiceReference;
+namespace YourWeather
+{
+    public partial class formWeather : Form
+    {
+        internal Cities.NewDataSet cn;
+        public formWeather()
+        {
+            InitializeComponent();
+
+            BasicHttpBinding binding = new BasicHttpBinding();
+            binding.MaxReceivedMessageSize = 200000000;
+
+            EndpointAddress address = new EndpointAddress("http://www.webservicex.com/globalweather.asmx");
+
+            GlobalWeatherSoapClient gwsc = new GlobalWeatherSoapClient(binding, address);
+
+            var cities = gwsc.GetCitiesByCountry("");
+
+            XmlSerializer result = new XmlSerializer(typeof(Cities.NewDataSet));
+            cn = (Cities.NewDataSet)result.Deserialize(new StringReader(cities));
+
+            var Countries = cn.Table.Select(m => m.Country).Distinct();
+
+            comboBoxCountry.Items.AddRange(Countries.ToArray());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var rr = cn.Table.Where(m => m.Country == comboBoxCountry.Text).Select(c => c.City);
+
+            comboBoxCities.Items.Clear();
+            comboBoxCities.Items.AddRange(rr.ToArray());
+        }
+
+        private void comboBoxCities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+
+            EndpointAddress address = new EndpointAddress("http://www.webservicex.com/globalweather.asmx?WSDL");
+
+            GlobalWeatherSoapClient gwsc = new GlobalWeatherSoapClient(binding, address);
+
+            var weather = gwsc.GetWeather(comboBoxCities.Text, comboBoxCountry.Text);
+
+            if(weather == "Data Not Found!")
+            {
+                richTextBoxWeatherDetails.Clear();
+
+                //XmlSerializer result = new XmlSerializer(typeof(CurrentWeather));
+
+                //var w = (CurrentWeather)result.Deserialize(new StringReader(weather));
+
+                //for( int i = 0; i< w.ItemsElementName.Lenght; i++)
+                //{
+                //    richTextBoxWeatherDetails.Text += w.ItmesElementName[i]+ ":" w.Items[i] + "\r\n";
+                //}
+            }
+            else
+            {
+                richTextBoxWeatherDetails.Clear();
+               richTextBoxWeatherDetails.Text = "Data Not Found!";
+            }
+        }
+    }
+}
